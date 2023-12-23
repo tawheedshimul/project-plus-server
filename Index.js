f1e497419a565150e3817154b813c5ed98e2559e
unknown <you@example.com>
2023-12-23 15:14:01 +0600

    updayte

--- Index.js
+++ Index.js
@@ -32,7 +32,7 @@ const verifyJWT = (req, res, next) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gro8ftq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${project-plus}:${project-plus}@cluster0.gro8ftq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
